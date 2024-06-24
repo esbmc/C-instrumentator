1f84622b628272da2fa0c92dd9f6cff756dbfecd
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2024-06-24 11:26:20 +0100

    updated catch2 version

--- scripts/cmake/ExternalDependencies.cmake
+++ scripts/cmake/ExternalDependencies.cmake
@@ -1,5 +1,7 @@
 include(CPM)
 CPMAddPackage("gh:fmtlib/fmt#7.1.3")
-CPMAddPackage("gh:catchorg/Catch2@3.0.1")
+
+# BUG: Removing catch will result in link problems with LLVM. I have no clue for the reason why
+CPMAddPackage("gh:catchorg/Catch2@3.6.0")
 
 include(FindLLVM)
