451eb07da9fe8b004ea99e5e0dd6f693fb7cef4d
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2024-06-25 11:38:03 +0100

    moved goal injector main executable

---
+++ src/goal-injector/main.cpp
@@ -0,0 +1,11 @@
+#include <goal-injector/goal-injector.h>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 2) 
+    std::cerr << "./injector <input.c> <output.c>\n";
+  const std::ifstream input(argv[1]);
+  GoalInjector::RunGoalInjectorA(input, argv[2]);
+  return 0;
+}
